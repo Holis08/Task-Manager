695054dae7035f28a99fa91fce84f8f964821dd7
Arielnazril <arielnazril133@gmail.com>
2024-10-28 17:47:05 +0700

    Menambahkan API dasar untuk pengelolaan tugas

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // Middleware untuk membaca JSON
-app.use("/tasks", taskRoutes); // Menggunakan routes untuk tasks
+app.use(express.json());
+app.use("/tasks", taskRoutes);
 
 mongoose
-  .connect("mongodb://localhost:27017/task_manager", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/task_manager")
   .then(() => console.log("Terkoneksi ke MongoDB"))
   .catch((error) => console.error("Koneksi gagal:", error));
 
-app.listen(PORT, () => {
-  console.log("Server berjalan di http://localhost:${PORT}");
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log("Server berjalan di http://localhost:${PORT}");
+  });
+}
+
+module.exports = app;
